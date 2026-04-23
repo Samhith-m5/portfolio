811c158cc17b54d092412a3963458959942efe45
Samhith Reddy <samhith@Samhiths-MacBook-Pro.local>
2026-04-23 18:34:53 +0530

    Implement global ScrollToTop on route change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Grid3D from './components/Grid3D';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import { useLocation } from 'react-router-dom';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    document.documentElement.style.scrollBehavior = 'auto';
+    window.scrollTo(0, 0);
+    setTimeout(() => {
+      document.documentElement.style.scrollBehavior = 'smooth';
+    }, 0);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <div className="app">
         <Grid3D />
         <Navbar />
